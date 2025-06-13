be167425cc116f891823d8cebbe96b2f989c7433
khyaejin <olivia019182@naver.com>
2025-06-13 16:28:31 +0900

    🎉 Start Project

---
+++ IosProject-hello/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  IosProject-hello
+//
+//  Created by 김혜진 on 6/13/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
