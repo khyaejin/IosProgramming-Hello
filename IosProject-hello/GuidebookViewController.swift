141206def64fc2d6939c7845bb2b232f93155638
khyaejin <olivia019182@naver.com>
2025-06-15 12:01:47 +0900

    🚧 Progress: GuideBook 화면 틀 구현 중...

---
+++ IosProject-hello/GuidebookViewController.swift
@@ -0,0 +1,19 @@
+//
+//  GuidebookViewController.swift
+//  IosProject-hello
+//
+//  Created by 김혜진 on 6/13/25.
+//
+
+import UIKit
+
+class GuidebookViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
